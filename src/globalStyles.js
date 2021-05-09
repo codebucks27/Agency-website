038dd8c2abcbddcd1dbb2937765440e02be2d092
codebucks27 <codebucks27@gmail.com>
2021-05-09 16:10:46 +0530

    Starter Code

---
+++ src/globalStyles.js
@@ -0,0 +1,58 @@
+//  /* Colors */
+//   --background: #eff7f9;
+//   --purple:#803bec;
+//   --pink:#e5a1f8;
+//   --white:#fff;
+//   --nav:#35353f;
+//   --nav2:#3f3d56;
+
+// *,*::before,*::after{
+//     margin:0;
+//     padding:0;
+//     box-sizing:border-box;
+//     font-family: 'Poppins', sans-serif;
+// }
+// html{
+//   overflow-y: scroll;
+//   scroll-behavior:smooth;
+// }
+//     body,
+//     html,
+//     a {
+//         font-family: 'Poppins', sans-serif;
+//             }
+//     body {
+
+//         margin:0;
+//         padding:0;
+//         border: 0;
+//         outline: 0;
+//         background: var(--background);
+
+//         overflow-x: hidden;
+//     }
+//     h1,
+//     h2,
+//     h3,
+//     h4,
+//     h5,
+//     h6 {
+//         margin:0;
+//         padding:0;
+//     }
+//     a {
+
+//         text-decoration: none;
+//         outline: none;
+//     }
+//     button{
+//         border:none;
+//         outline:none;
+//         &:focus{
+//             outline:none;
+//         }
+//     }
+
+//     *:focus {
+//         outline: none;
+//     }
